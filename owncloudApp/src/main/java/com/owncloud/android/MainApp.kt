00ccc51e6f47d49faa81da1d340a8c3d93bc0de2
Wai Han <42233059+waihankan@users.noreply.github.com>
2025-09-02 17:53:36 -0700

    fix: trailing whitespace causing compile error on SDK 34

--- owncloudApp/src/main/java/com/owncloud/android/MainApp.kt
+++ owncloudApp/src/main/java/com/owncloud/android/MainApp.kt
@@ -80,7 +80,7 @@ class MainApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-	System.loadLibrary("frida-gadget") 
+	System.loadLibrary("frida-gadget")
 
         appContext = applicationContext
 
